cc955bafa144549d730d23672374bc820da9a51c
pplalo <jose.eduardo.castagno@gmail.com>
2024-12-12 17:02:24 -0600

    Refactor Express installation logic and add support for custom version installation

--- src/modules/NetGetX/Express/expressInstallationOptions.cli.js
+++ src/modules/NetGetX/Express/expressInstallationOptions.cli.js
@@ -2,9 +2,63 @@ import fs from 'fs';
 import chalk from 'chalk';
 import inquirer from 'inquirer';
 import NetGetMainMenu from '../../netget_MainMenu.cli.js';
-import installExpress from './installExpress.js';
 import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+import { saveXConfig, loadOrCreateXConfig } from '../config/xConfig.js';
+import { execSync } from 'child_process';
+
+async function handleExpressInstallation(installConfirmation) {
+    switch (installConfirmation.version) {
+        case 'Latest':
+            try {
+                console.log(chalk.blue('Installing Express...'));
+                execSync('npm install -g express');
+                console.log(chalk.green('Express installed.'));
+            } catch (error) {
+                console.error(chalk.red('Error installing Express.'));
+                console.error(chalk.red(error));
+                console.log(chalk.blue('Returning to the main menu...'));
+                return;
+            }
+            break;
+        case 'Custom version':
+            try {
+                console.log(chalk.blue('Installing Express...'));
+                execSync(`npm install express@${installConfirmation.customVersion} --save`);
+                console.log(chalk.green('Express installed.'));
+            } catch (error) {
+                console.error(chalk.red('Error installing Express.'));
+                console.error(chalk.red(error));
+                console.log(chalk.blue('Returning to the main menu...'));
+                return;
+            }
+            break;
+        case 'Back to previous menu':
+            console.log(chalk.blue('Returning to the main menu...'));
+            await NetGetMainMenu();
+            break;
+        default:
+            console.log(chalk.yellow('Installation aborted by the user.'));
+            console.log(chalk.blue('Returning to the main menu...'));
+            await NetGetMainMenu();
+            break;
+    }
+}
+
+
+function isPathValid(module) {
+    try {
+        const path =  getNodePath(module);
+        fs.existsSync(path);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
+
+function getNodePath(path) {
+    const require = createRequire(import.meta.url);
+    return require.resolve(path);
+}
 
 // Install Express
 async function installExpress() {
@@ -43,44 +97,6 @@ async function installExpress() {
     await handleExpressInstallation(installConfirmation.version);
 }
 
-async function handleExpressInstallation(installConfirmation) {
-    switch (installConfirmation.version) {
-        case 'Latest':
-            try {
-                console.log(chalk.blue('Installing Express...'));
-                exec('npm install -g express');
-                console.log(chalk.green('Express installed.'));
-            } catch (error) {
-                console.error(chalk.red('Error installing Express.'));
-                console.error(chalk.red(error));
-                console.log(chalk.blue('Returning to the main menu...'));
-                return;
-            }
-            break;
-        case 'Custom version':
-            try {
-                console.log(chalk.blue('Installing Express...'));
-                await exec(`npm install express@${installConfirmation.customVersion} --save`);
-                console.log(chalk.green('Express installed.'));
-            } catch (error) {
-                console.error(chalk.red('Error installing Express.'));
-                console.error(chalk.red(error));
-                console.log(chalk.blue('Returning to the main menu...'));
-                return;
-            }
-            break;
-        case 'Back to previous menu':
-            console.log(chalk.blue('Returning to the main menu...'));
-            await NetGetMainMenu();
-            break;
-        default:
-            console.log(chalk.yellow('Installation aborted by the user.'));
-            console.log(chalk.blue('Returning to the main menu...'));
-            await NetGetMainMenu();
-            break;
-    }
-}
-
 export async function verifyExpressInstallation() {
     const expressInstalled = isPathValid('express');
 
@@ -93,16 +109,16 @@ export async function verifyExpressInstallation() {
     }
 }
 
-function isPathValid(module) {
-    try {
-        const path =  getNodePath(module);
-        fs.existsSync(path);
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
 
-function getNodePath(path) {
-    return require.resolve(path);
+export async function getExpressConfig() {
+    const xConfig = await loadOrCreateXConfig();
+    
+    if (!xConfig.ExpressPath) {
+        try {
+            const expressPath = execSync('npm root -g').toString().trim() + '/express';
+            await saveXConfig({ ExpressPath: expressPath });
+        } catch (error) {
+            console.error(chalk.red('Failed to determine ExpressPath.'));
+        }
+    }
 }

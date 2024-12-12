51390e3fb2a016513e649b6a43969ef563c75777
pplalo <jose.eduardo.castagno@gmail.com>
2024-12-12 12:45:03 -0600

    Acesspoints deletion

--- src/modules/AccessPoints/AccessPoints.js
+++
@@ -1,9 +0,0 @@
-// Path: netget/src/modules/AccessPoints/AccessPoints.js
-import chalk from 'chalk';
-
-export function handleAccessPoints() {
-  console.log(chalk.yellow('Interacting AccessPoints..'));
-  // NetGetX logic here
-}
-
-

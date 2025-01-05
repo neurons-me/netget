ee9824b7ce2883f03ccb69459bfd805a6b0f2145
pplalo <jose.eduardo.castagno@gmail.com>
2025-01-04 21:54:06 -0600

    Add Srvrs CLI module and integrate into main menu for port services

---
+++ src/modules/Srvrs/srvrs.cli.js
@@ -0,0 +1,23 @@
+// netget/src/modules/Srvrs/srvrs.cli.js
+
+/**
+ * Displays the Gateways Menu and handles user input.
+ * @category Gateways
+ * @subcategory Main    
+ * @module Srvrs_CLI
+ */
+
+export async function Srvrs_CLI() {
+    console.clear();
+    console.log(`
+             __________________ 
+            |   SRVRS PORTS    |---->>>
+            |_______.P.________|---->>>
+HTTPS--->>> |_______.R.________|---->>>  
+            |_______.O.________|---->>>
+            |_______.X.________|---->>>
+            |_______.Y.________|---->>>
+     `);
+};
+
+export default Srvrs_CLI;
--- src/modules/netget_MainMenu.cli.js
+++ src/modules/netget_MainMenu.cli.js
@@ -2,66 +2,60 @@
 import inquirer from 'inquirer';
 import chalk from 'chalk';
 import { i_DefaultNetGetX } from './NetGetX/config/i_DefaultNetGetX.js';
-import  NetGetX_CLI  from './NetGetX/NetGetX.cli.js';
-import { i_DefaultGateway } from './Gateways/config/i_DefaultGateway.js';
-import { App_CLI } from './Gateways/gateways.cli.js';
+import NetGetX_CLI from './NetGetX/NetGetX.cli.js';
+import { Srvrs_CLI } from './Srvrs/srvrs.cli.js';
 import { PortManagement_CLI } from './PortManagement/portManagement.cli.js';
-//import { handleAccessPoints } from './AccessPoints/AccessPoints.js';
-//import { handleGets } from './Gets/Gets.js';
 /**
  * the NetGet CLI.
  */
 export default async function NetGetMainMenu() {
+    console.clear();
     console.log(`
     ╔╗╔┌─┐┌┬┐╔═╗┌─┐┌┬┐
     ║║║├┤  │ ║ ╦├┤  │ 
     ╝╚╝└─┘ ┴ ╚═╝└─┘ ┴ 
         v2.5`);
     const answers = await inquirer.prompt([
-    {
-        type: 'list',
-        name: 'action',
-        message: 'Main Menu',
-        choices: [
-            'X (HTTPS, Domains and Routes)',
-            '/srv/ (Port Services)',
-            '/var/www (Static)',
-            new inquirer.Separator(),
-            'Port Management',
-            new inquirer.Separator(),
-            'Exit',
-            new inquirer.Separator()],
-    },
+        {
+            type: 'list',
+            name: 'action',
+            message: 'Main Menu',
+            choices: [
+                'X (HTTPS, Domains and Routes)',
+                'Srvrs - (Port Services)',
+                'Statics - (Static files)',
+                new inquirer.Separator(),
+                'Port Management',
+                new inquirer.Separator(),
+                'Exit',
+                new inquirer.Separator()],
+        },
     ]);
 
     switch (answers.action) {
         case 'X (HTTPS, Domains and Routes)':
             const x = await i_DefaultNetGetX();
             if (x) {
-                    await NetGetX_CLI(x); 
-                    } else {
-                    console.log(chalk.red('Setup verification failed. Please resolve any issues before proceeding.'));
-                    }
+                /*
+                Netget X (The Router/Conductor)
+                Role: Acts as the central orchestrator,
+                running an Nginx server and managing domain routing.
+                */
+                await NetGetX_CLI(x);
+                break;
+            } else {
+                console.log(chalk.red('Setup verification failed. Please resolve any issues before proceeding.'));
+            }
             break;
 
-            case '/srv/ (Port Services)':
-                const g = await i_DefaultGateway(); //Load production configuration
-                if (g) {
-                    console.log(`
-                       __________________ 
-                      |   The GATEWAY    |---->>
-                      |_______...________|
-      PORT:3432--- >>>|_______...________|---->>>  ${chalk.green(g)}
-                      |_______...________|---->>>
-                      |_______...________|---->>>
-                    `);
-                    await App_CLI(g);  // Pass the development flag to the CLI
-                } else {
-                    console.log(chalk.red('Setup verification failed. Please resolve any issues before proceeding.'));
-                }
-                break;
+        case 'Srvrs - (Port Services)':
+            /*
+            Role: Manages and adds backend services listening on specific ports.
+            */
+            await Srvrs_CLI();
+            break;
 
-        case '/var/www (Static)':
+        case 'Statics - (Static files)':
             console.log(chalk.yellow('Selected Gets'));
             // Call Gets functionality here
             break;
@@ -69,7 +63,7 @@ export default async function NetGetMainMenu() {
         case 'Port Management':
             await PortManagement_CLI();
             break;
-    
+
         case 'Exit':
             console.log(chalk.green('Exiting NetGet CLI.'));
             process.exit();

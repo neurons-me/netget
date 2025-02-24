4149ab41845605f58647e86597d65369ffcf2a1c
José E. Castaño <110692825+pplalo@users.noreply.github.com>
2025-02-24 10:05:41 -0600

    Version 2.6.1

--- src/modules/netget_MainMenu.cli.js
+++ src/modules/netget_MainMenu.cli.js
@@ -57,7 +57,7 @@ export default async function NetGetMainMenu() {
     ╔╗╔┌─┐┌┬┐╔═╗┌─┐┌┬┐
     ║║║├┤  │ ║ ╦├┤  │ 
     ╝╚╝└─┘ ┴ ╚═╝└─┘ ┴ 
-        v2.6.0`);
+        v2.6.1`);
     const answers = await inquirer.prompt([
         {
             type: 'list',

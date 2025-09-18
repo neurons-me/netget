0e9e0e0deee569763612ccccd827dd136657957a
pplalo <jose.eduardo.castagno@gmail.com>
2025-09-18 12:13:30 -0600

    2.6.45 add missing file for the index.js file

---
+++ src/netget.js
@@ -0,0 +1,12 @@
+// src/netget.js
+class NetGet {
+  /**
+   * Constructs the NetGet service, initializing any necessary base configurations.
+   * @constructor
+   * @param {Object} config - The configuration object for the NetGet service.
+   */
+  constructor() {
+    // Initialization code, if necessary
+  }
+}
+export default NetGet;
